8ec2a239ac8039c1a024a3851be4fe3a5afef652
Jacob Mendoza <jacob.mendoza.hdz@gmail.com>
2022-12-01 09:58:56 -0600

    sw

--- sw.js
+++ sw.js
@@ -16,25 +16,16 @@ self.addEventListener("install", (e) => {
 });
 
 
-self.addEventListener('fetch', e =>{
-    //cache with network fallback
-    const respuesta = caches.match( e.request )
-        .then ( res => {
-            if ( res ) return res;
-            //no existe el archivo
-            //tengo que ir a la web
-            console.log('No existe', e.request.url);
-            return fetch( e.request ).then ( newResp => {
-                caches.open('cache-v1')
-                    .then( cache => {
-                        cache.put( e.request, newResp);
-                    }
-
-                    )
-                return newResp.clone;
-            });
-        });
-        e.respondWith(respuesta);
-    //only cache
-    //e.respondWith( caches.match(e.request));
+//Evento fetch
+self.addEventListener("fetch", event => {
+	event.respondWith(
+		caches.open('cache-v1').then(cache => {
+			return cache.match(event.request).then(response => {
+				return response || fetch(event.request).then(networkResponse => {
+					cache.put(event.request, networkResponse.clone());
+					return networkResponse;
+				});
+			})
+		})
+	);
 });
