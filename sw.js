af15ad64ce826a77661b60c634eaed9923bf108a
Jacob Mendoza <jacob.mendoza.hdz@gmail.com>
2022-12-01 08:52:23 -0600

    first commit

---
+++ sw.js
@@ -0,0 +1,40 @@
+self.addEventListener("install", (e) => {
+  caches.open("cache-v1").then((cache) => {
+    cache.addAll([
+      "index.html",
+      "main.js",
+      "images/poeta1.jpg",
+      "images/poeta2.jpg",
+      "images/poeta3.jpg",
+      "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css",
+      "videos/7-hispanos.mp4",
+      "videos/10-poemas.mp4",
+      "videos/Antonio-MACHADO.mp4",
+    ]);
+  });
+  e.waitUntil(cacheProm);
+});
+
+
+self.addEventListener('fetch', e =>{
+    //cache with network fallback
+    const respuesta = caches.match( e.request )
+        .then ( res => {
+            if ( res ) return res;
+            //no existe el archivo
+            //tengo que ir a la web
+            console.log('No existe', e.request.url);
+            return fetch( e.request ).then ( newResp => {
+                caches.open('cache-v1')
+                    .then( cache => {
+                        cache.put( e.request, newResp);
+                    }
+
+                    )
+                return newResp.clone;
+            });
+        });
+        e.respondWith(respuesta);
+    //only cache
+    //e.respondWith( caches.match(e.request));
+});
